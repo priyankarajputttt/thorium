9d54d9cea7c119ef2c7f29aad6849d80eb7e54bf
priyankarajputttt <pr639490@gmail.com>
2022-03-09 03:01:18 +0530

    ok

---
+++ thorium-session-auth-1/src/models/loginModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const loginSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+}, { timestamps: true })
+
+module.exports = mongoose.model('loginUser', loginSchema)
